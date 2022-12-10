0b8abb331c121d85bb242fb1f8c58dd01c07a626
Seyedmakhtoom <120266917+Seyedmakhtoom@users.noreply.github.com>
2022-12-11 00:26:54 +0330

    Add files via upload

--- A1.py
+++ A1.py
@@ -1,26 +1,48 @@
 import math
 
+t = input("Please choose which operation do you ccarry out: ") 
+if t == "1":
+    x = float(input("Please enter the number: "))
+    op = input("Please choose operation: ")
 
-a = float(input("Please enter a number: "))
+    if op == "sine":
+        result = math.sin(math.radians(x))
+    if op == "cos":
+        result = math.cos(math.radians(x))
+    if op == "tan":
+        result = math.tan(math.radians(x))
+    if op == "cot":
+        result = math.cot(math.radians(x))
+    if op == "r" and x > 0:
+        result = math.sqrt(x)
+    else:
+        print("Error")
+    if op == "f":
+        if x == 0 and x == 1:
+            print("1")
+        x = int(x);
+        result = math.factorial(x)
+    
+    print(result)
+ 
+if t == "2":
+    a = float(input("Please enter the number: "))
+    b = float(input("Please enter the number: "))
+    op = input("Pleas enter operation: ")
 
-op = input("Please enter operation: ")
-
-if op == "radical":
-    result = math.sqrt(a)
-if op == "sine":
-    result = math.sin(math.radians(a))
-if op == "cos":
-    result = math.cos(math.radians(a))
-if op == "cot":
-    result = math.cot(math.radians(a))
-if op == "tan":
-    result = math.tan(math.radians(a)) 
-if op == "factorial":
-    a= int(a);
-    result = math.factorial(a)
-
-
+if op == "+":
+    result = a + b 
+if op == "-":  
+    if a >= b:
+        result = a - b
+    else:
+        print("Error")
+if op == "*":
+    result = a * b 
+if op == "/":
+    if a >= b:
+        result = a / b
+    else:
+        print("Error") 
 
 print(result)
-
-
--- A2.py
+++ A2.py
@@ -6,7 +6,7 @@ a = float(input("Please enter your first dimension: "))
 b = float(input("Please enter your second dimension: "))
 c = float(input("Please enter your third dimension: "))
 
-if a <= b + c and b <= a + c and c <= a + b:
+if a < b + c and b < a + c and c < a + b:
    print(color.Yellow + "Valid dimension ")
 else:  
     print(color.Yellow + "Invalid dimension ")
